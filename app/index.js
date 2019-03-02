e4fb5f08c9af7efad7df485416d2481f28cfe9f6
Cody <244635969@qq.com>
2019-03-02 14:33:01 +0800

    feat: react

--- app/index.js
+++ app/index.js
@@ -1,2 +1,7 @@
-const greet = require('./Greeter')
-document.querySelector('#app').appendChild(greet())
+// const greet = require('./Greeter')
+// document.querySelector('#app').appendChild(greet())
+import React from 'react';
+import { render } from 'react-dom';
+import Greeter from './Greeter';
+
+render(<Greeter/>, document.getElementById('#app'))
--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: [
-              "env", "react"
+              '@babel/react', '@babel/env'
             ]
           }
         },
